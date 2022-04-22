10e2d20567d21d8fa2e0b344772a1d03fc72e7d6
aconkli9 <aconkli9@ford.com>
2022-04-22 14:49:45 -0400

    Add row addition logic

--- index.js
+++ index.js
@@ -3,14 +3,34 @@ const form = document.querySelector('#add-item')
 form.addEventListener('submit', (event) => {
     event.preventDefault()
     const formData = new FormData(event.target)
-    const currentInventory = document.querySelector('#current-inventory')
-    const newRow = document.createElement('tr')
-    newRow.innerHTML = `
-        <th>${formData.get('item-name')}</th>
-        <th>${formData.get('item-sell-in')}</th>
-        <th>${formData.get('item-quality')}</th>
-        <th>${formData.get('date-added')}</th>
-    `
-    currentInventory.append(newRow)
+    const newItem = {
+        name: formData.get('item-name'),
+        sellIn: formData.get('item-sell-in'),
+        startingQuality: formData.get('item-quality'),
+        dateAdded: formData.get('date-added')
+    }
     form.reset()
+
+    localStorage.setItem('newItem', JSON.stringify(newItem))
 })
+
+const newItem = JSON.parse(localStorage.getItem('newItem'))
+const newItemName = newItem.name
+const dateNewItemAdded = new Date(`${newItem.dateAdded}`)
+
+function calculateElapsedDays(date) {
+    const dateNewItemAddedInMill = dateNewItemAdded.getTime()
+    const currentDateInMill = Date.now()
+    const elapsedMill = currentDateInMill - dateNewItemAddedInMill
+    return elapsedDays = Math.floor((elapsedMill / (60 * 60 * 24 * 1000)))
+}
+
+const currentInventory = document.querySelector('#current-inventory')
+const newRow = document.createElement('tr')
+newRow.innerHTML = `
+        <th>${newItem.name}</th>
+        <th>${+newItem.sellIn - (calculateElapsedDays(newItem.dateAdded))}</th>
+        <th>${newItem.startingQuality - (calculateElapsedDays(newItem.dateAdded))}</th>
+        <th>${newItem.dateAdded}</th>
+    `
+currentInventory.append(newRow)
